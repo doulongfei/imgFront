5f5f19af56d577bca0acb9f72a0ad2250440567b
lfdou <2648142550@qq.com>
2024-07-27 18:41:46 +0800

    上传失败详情

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -162,8 +162,7 @@ methods: {
     },
     handleSuccess(response, file) {
         try {
-            const rootUrl = `${window.location.protocol}//${window.location.host}`
-            this.fileList.find(item => item.uid === file.uid).url = rootUrl + response.data[0].src
+            this.fileList.find(item => item.uid === file.uid).url = response.data[0].src
             this.fileList.find(item => item.uid === file.uid).progreess = 100
             this.fileList.find(item => item.uid === file.uid).status = 'success'
             this.$message({
@@ -174,7 +173,7 @@ methods: {
                 this.fileList.find(item => item.uid === file.uid).status = 'done'
             }, 3000)
         } catch (error) {
-            this.$message.error(file.name + '上传失败')
+            this.$message.error(file.name + '上传失败' + JSON.stringify(error))
             this.fileList.find(item => item.uid === file.uid).status = 'exception'
         } finally {
             if (this.uploadingCount + this.waitingCount === 0) {
